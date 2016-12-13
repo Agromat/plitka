37014a6229a3d50b19cf419eda95282f7cf06b25
Max Kirigetov <maximkirigetov@gmail.com>
2016-12-13 18:11:58 +0200

    mobile menu, mobile tabs

--- js/app.js
+++ js/app.js
@@ -64,13 +64,13 @@
         });
       }
 
-      _hideNavOnClick() {
-        $('body').click((e) => {
-          if ( !$(e.target).closest(this.btn).length && !$(e.target).is(this.nav) ) {
-            this._hideNav();
-          }
-        });
-      }
+      // _hideNavOnClick() {
+      //   $('body').click((e) => {
+      //     if ( !$(e.target).closest(this.btn).length && !$(e.target).is(this.nav) ) {
+      //       this._hideNav();
+      //     }
+      //   });
+      // }
 
       _hideNavOnResize() {
         if ( $(window).width() > 1280 && this.nav.hasClass('is-visible') ) {
@@ -89,7 +89,7 @@
 
       init() {
         this._showNav();
-        this._hideNavOnClick();
+        // this._hideNavOnClick();
       }
     }
 
@@ -114,7 +114,73 @@
     if ( !isTouchDevice() ) { $('body').addClass('no-touch') };
 
 
+    // show/hide dropdown in navigation
+    function initSubmenuToggle() {
+      var navLink = $('.js-nav-link'),
+        subnavLink = $('.js-subnav-link'),
+        // navOption = $('.js-nav-option'),
+        navDrop = $('.js-navdrop'),
+        subDrop = $('.js-subnav-drop'),
+        active = 'is-active';
 
+      var hideCurrentNavDrop = function() {
+        navLink.removeClass(active);
+        navDrop.removeClass(active);
+      };
+
+      var showNavDrop = function(el) {
+        $(el).addClass(active);
+        $(el).parent().find(navDrop).addClass(active);
+      };
+
+      navLink.click(function(evt) {
+
+
+        if ( $(window).width() < 768 ) {
+          if ( $(this).siblings(navDrop).length ) {
+            evt.preventDefault();
+            var isActive = $(this).hasClass(active);
+
+            hideCurrentNavDrop();
+            if ( !isActive ) showNavDrop($(this));
+          } else {
+            hideCurrentNavDrop();
+            $(this).addClass(active);
+          }
+        }
+        else{
+          if ( $(this).siblings(navDrop).length ) {
+            evt.preventDefault();
+            var isActive = $(this).hasClass(active);
+
+            hideCurrentNavDrop();
+            if ( !isActive ) $(this).addClass(active);
+          } else {
+            hideCurrentNavDrop();
+            $(this).addClass(active);
+          }
+        }
+
+
+      });
+
+      subnavLink.click(function(evt) {
+        if ( $(this).siblings(subDrop).length ) {
+          evt.preventDefault();
+
+          $(this).toggleClass(active);
+          $(this).siblings(subDrop).toggleClass(active);
+        }
+      });
+      
+      $(window).resize(function() {
+        if ( desktop() && navDrop.hasClass(active) ) {
+          hideCurrentNavDrop();
+        }
+      });
+    }
+
+    initSubmenuToggle();
 
 
 
